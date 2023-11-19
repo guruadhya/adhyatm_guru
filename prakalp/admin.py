ca530be85926577450b2ea80f1c11405b2083377
sanjay <sanjaypython22@gmail.com>
2023-11-19 23:30:36 +0530

    kjhgfds

--- prakalp/admin.py
+++ prakalp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Prakalp, PrakalpImage
 
 # Register your models here.
+admin.site.register(Prakalp)
+admin.site.register(PrakalpImage)
--- site_settings/models.py
+++ site_settings/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class SiteSettings(models.Model):
+    site_name = models.CharField(max_length=256)
+    site_logo = models.ImageField(upload_to='site_logo')
+    site_description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.site_name
+
+    class Meta:
+        db_table = 'site_settings'
