ef578a635611b2ba4aa7412e00a7a34dd699ce88
sanjay <sanjaypython22@gmail.com>
2023-11-20 00:39:01 +0530

    kjhgfds

--- prakalp/views.py
+++ prakalp/views.py
@@ -8,6 +8,7 @@ from .models import Prakalp, PrakalpImage
 def prakalp_details(request, prakalp_id):
     prakalp = Prakalp.objects.get(id=prakalp_id)
     prakalp_images = PrakalpImage.objects.filter(prakalp_id=prakalp_id)
+    # print(prakalp_images, '============prakalp_images')
     context = {
         'prakalp': prakalp,
         'images': prakalp_images,
