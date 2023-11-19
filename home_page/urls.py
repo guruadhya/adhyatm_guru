a9189f2766c766102949f4a0fc69bc4cd5c69763
sanjay <sanjaypython22@gmail.com>
2023-11-19 23:53:35 +0530

    kjhgfds

--- home_page/urls.py
+++ home_page/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page, name='home_page'),
+    path('prakalp_details/<int:prakalp_id>/', views.prakalp_details, name='prakalp_details'),
 
 ]
--- home_page/views.py
+++ home_page/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from prakalp.models import Prakalp
+from prakalp.models import Prakalp, PrakalpImage
 
 
 def home_page(request):
@@ -10,3 +10,13 @@ def home_page(request):
         'prakalp': prakalp
     }
     return render(request, 'home_page.html', context)
+
+
+def prakalp_details(request, prakalp_id):
+    prakalp = Prakalp.objects.get(id=prakalp_id)
+    prakalp_images = PrakalpImage.objects.filter(prakalp_id=prakalp_id)
+    context = {
+        'prakalp': prakalp,
+        'images': prakalp_images,
+    }
+    return render(request, 'prakalp_details.html')
