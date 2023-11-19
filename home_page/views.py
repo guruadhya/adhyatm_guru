9dee9f58d4a21faf7da6e055824358d3a08ba3b8
sanjay <sanjaypython22@gmail.com>
2023-11-19 23:42:43 +0530

    kjhgfds

--- home_page/views.py
+++ home_page/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from adhyatm_guru.prakalp.models import Prakalp
+
+
 def home_page(request):
-    return render(request, 'home_page.html')
+    prakalp = Prakalp.objects.all()
+    context = {
+        'prakalp': prakalp
+    }
+    return render(request, 'home_page.html', context)
