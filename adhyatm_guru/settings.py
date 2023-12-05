3390f133380d1087577b626d8467e30859404a27
sanjay <sanjaypython22@gmail.com>
2023-12-05 22:56:27 +0530

    kjhgf

--- adhyatm_guru/settings.py
+++ adhyatm_guru/settings.py
@@ -65,10 +65,21 @@ WSGI_APPLICATION = 'adhyatm_guru.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'adhyatm',
+        'USER': 'root',
+        'PASSWORD': 'Hello123456#',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
