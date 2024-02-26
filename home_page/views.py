ffe9feb69acf6924bcf302e28b88e44dd202913c
nidhivan ras <nidhivanras@gmail.com>
2024-02-26 22:10:03 +0530

    p

--- home_page/views.py
+++ home_page/views.py
@@ -44,20 +44,6 @@ def home_page(request):
     else:
         dan1 = ''
 
-    # api_key = 'AIzaSyCJQ2WoCt9gMmdKlkaRS_NqEyNeNyxDm9k'
-    # youtube = build('youtube', 'v3', developerKey=api_key)
-    #
-    # # Get the latest videos from a specific channel (replace 'CHANNEL_ID' with your channel ID)
-    # channel_id = 'UCH-cK1RIBzsbVlNnWDD0nHw'
-    # response = youtube.search().list(
-    #     part='snippet',
-    #     channelId=channel_id,
-    #     order='date',
-    #     type='video',
-    #     maxResults=9  # You can adjust the number of videos to retrieve
-    # ).execute()
-    #
-    # videos = response.get('items', [])
     context = {
         'site_info': site_info[0],
         'header_banner_homepage': header_banner_homepage,
